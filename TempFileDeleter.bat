3ce30ae7ca88dddc8538a74ff5a88f04a87d5ebf
Shourjjo Majumder <shourjjo.majumder@protonmail.com>
2022-02-03 10:25:57 +0530

    updated the file and make it work better

--- TempFileDeleter.bat
+++ TempFileDeleter.bat
@@ -1,9 +1,12 @@
 @echo off
 color 3
+echo version 1.1.0
+echo please visit our repo https://www.github.com/shourgamer2/TempFileRemover
 echo Temp File deleter
 title Temp File deleter
 cd /D %temp%
 for /d %%D in (*) do rd /s /q "%%D"
 del /f /q *
 echo Temp Files Have Been Deleted
+start https://www.github.com/shourgamer2/TempFileRemover
 pause
